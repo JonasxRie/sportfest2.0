001eb0592f757e68e1792c45e461b8a8fe70380b
JonasxRie <jonasrieskamp@googlemail.com>
2017-06-20 19:34:56 +0200

    Einbinden des Menüs in die Header Component

--- sportfest/src/app/dashboard/dashboard.component.ts
+++ sportfest/src/app/dashboard/dashboard.component.ts
@@ -8,6 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  teilnehmer = [
+    {
+      name: 'Mirco',
+      value: 12.5
+    },
+    {
+      name: 'Maja',
+      value: 12.5
+    },
+    {
+      name: 'Michi',
+      value: 12.5
+    },
+    {
+      name: 'Maxi',
+      value: 12.5
+    },
+    {
+      name: 'Jonas',
+      value: 12.5
+    },
+    {
+      name: 'David',
+      value: 12.5
+    }
+  ];
+
   constructor(private router: Router) { }
 
   ngOnInit() {
--- sportfest/src/app/header/header.component.ts
+++ sportfest/src/app/header/header.component.ts
@@ -27,4 +27,8 @@ export class HeaderComponent implements OnInit {
     this.router.navigate(['/team']);
   }
 
+  public navigateToDashboard() {
+    this.router.navigate(['/home']);
+  }
+
 }
