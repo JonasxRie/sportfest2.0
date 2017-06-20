12473d932111335395c8f5338c1342173d3acbe7
Michael Dunsche <michael.dunsche@s-atiw>
2017-06-20 11:33:25 +0200

    Routing

--- sportfest/src/app/app.module.ts
+++ sportfest/src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { EinzelComponent } from './einzel/einzel.component';
 import { TeamComponent } from './team/team.component';
 import { SportfestService } from './sportfest.service';
 import { TechnischerService } from './technischer.service';
+
+const routConfig: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: AppComponent },
+    { path: 'einzel', component: EinzelComponent },
+    { path: 'team', component: TeamComponent }
+];
 
 @NgModule({
   declarations: [
@@ -14,7 +22,8 @@ import { TechnischerService } from './technischer.service';
     TeamComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routConfig)
   ],
   providers: [
     TechnischerService,
