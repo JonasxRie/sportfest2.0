5f29d26cf1bb08b33f7e160623313ad9149d96cf
Michael Dunsche <michael.dunsche@s-atiw>
2017-06-23 15:04:01 +0200

    mobile menu list

--- sportfest/src/app/sportfest.service.ts
+++ sportfest/src/app/sportfest.service.ts
@@ -35,7 +35,7 @@ export class SportfestService {
    * Anmeldung
    */
   public userLogin(username: string, password: any): Observable<any> {
-    return this.techService.postRequest('/authentication', {username: username, password: password});
+    return this.techService.postRequest('/user/login', {username: username, password: password});
   }
   /**
    * Gibt die User Privilegien zurück
