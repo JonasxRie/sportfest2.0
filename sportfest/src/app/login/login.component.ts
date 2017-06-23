44940acd42093a50ce3cf006dda0b1f96299cbff
Michael Dunsche <michael.dunsche@s-atiw>
2017-06-23 15:12:37 +0200

    Verschlüsselung beim Passwort ändern

--- sportfest/src/app/login/login.component.ts
+++ sportfest/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Md5 } from 'ts-md5/dist/md5';
-import { SportfestService } from './../sportfest.service';
+import { SportfestService } from '../sportfest.service';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
