632bbba92a5ade2b763d725c616f575f86cf63f5
Michael Dunsche <michael.dunsche@s-atiw>
2017-06-23 09:38:13 +0200

    mobile menu and sidenav

--- sportfest/src/app/mobile-header/mobile-header.component.ts
+++ sportfest/src/app/mobile-header/mobile-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-mobile-header',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MobileHeaderComponent implements OnInit {
 
+  @Output() sidenavChange = new EventEmitter<any>();
+  
   constructor() { }
 
   ngOnInit() {
   }
-
+  
+  public changeSidenav() {
+    this.sidenavChange.emit();
+  }
 }
