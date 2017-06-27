eef4ff9cd68607cdc44829bd0ef9b9103f9d99b5
Mirco Boeckmann <mirco.boeckmann@s-atiw>
2017-06-27 14:11:18 +0200

    Disziplin aktivieren überarbeitet

--- sportfest/src/app/activate-discipline/activate-discipline.component.ts
+++ sportfest/src/app/activate-discipline/activate-discipline.component.ts
@@ -61,8 +61,8 @@ export class ActivateDisciplineComponent implements OnInit {
     // };
   }
 
-  public save(index: number, dis:Disziplin) {
-    this.sfService.disziplinAendern(index, dis).subscribe(data => {
+  public save(dis:Disziplin) {
+    this.sfService.disziplinAendern(dis.did, dis).subscribe(data => {
         
       },
       (err) => {
