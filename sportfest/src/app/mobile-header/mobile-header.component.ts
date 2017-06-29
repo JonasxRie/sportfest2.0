2c0df03c9952f696e24dec1c7e5e4b9b799db680
Michael Dunsche <michael.dunsche@s-atiw>
2017-06-29 07:47:40 +0200

    role mobile debug

--- sportfest/src/app/mobile-header/mobile-header.component.ts
+++ sportfest/src/app/mobile-header/mobile-header.component.ts
@@ -57,6 +57,7 @@ export class MobileHeaderComponent implements OnInit {
             this.username = null;
           }
           this.role = data.role;
+          console.log('emit Rolle --> ' + this.role);
           this.roleChanged.emit(this.role);
         },
         (err) => {
