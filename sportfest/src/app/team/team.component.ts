459384f117fc07e26bf506b607d7f8e949184cbb
Michael Dunsche <michael.dunsche@s-atiw>
2017-07-04 10:11:34 +0200

    team

--- sportfest/src/app/team/team.component.ts
+++ sportfest/src/app/team/team.component.ts
@@ -45,15 +45,13 @@ export class TeamComponent implements OnInit {
 
   ngOnInit() {
     this.route.params.forEach((params: Params) => {
-      let sportartID = params['did'];
-      
-      this.sfService.disziplin(sportartID).subscribe((data: Disziplin) => {
-        this.sportart = data;
+      this.did = params['did'];
+      this.sfService.disziplin(this.did).subscribe((data: Disziplin) => {
+        console.log('team-data');
+        console.log(data);
+        this.sportart = data.name;
         this.anzahlSpalten = new Array<number>(this.sportart.kontrahentenAnzahl);
         this.beschreibung = data.beschreibung;
-        
-        // Daten in die entsprechenden Felder füllen
-        console.log(data);
       },
       (err) => {
         console.error('GET-Service "disziplin(sportartID)" not reachable.');
@@ -74,46 +72,44 @@ export class TeamComponent implements OnInit {
     this.selectedClassB = null;
     this.pointsA = null;
     this.pointsB = null;
-    
     //Hier senden
-    this.sfService.ergebnisSchreiben(this.did, [{classA, pointsA}, {classB, pointsB}]) //TODO richtiges JSON
+    //TODO richtiges JSON
+    this.sfService.ergebnisSchreiben(this.did, [{classA, pointsA}, {classB, pointsB}]);
   }
   
-  public setVarValues(vid: number, ki: number, vi: number, event: any){
+  public setVarValues(vid: number, ki: number, vi: number, event: any) {
     console.log(event);
   }
 
+  // Sortieren
   switchSortA(){
     this.sortRevA = !this.sortRevA;
   }
   switchSortB(){
     this.sortRevB = !this.sortRevB;
-  }
-  
-  public sortByClassA(){
-    this.switchSortA()
-    this.punkteStand = this.punkteStand.sort((n1,n2)=>{
-      if(n1.classA>n2.classA){
+  }  
+  public sortByClassA() {
+    this.switchSortA();
+    this.punkteStand = this.punkteStand.sort((n1,n2)=> {
+      if (n1.classA>n2.classA) {
         return -1;
       }
-      if(n1.classA<n2.classA){
+      if (n1.classA<n2.classA) {
         return 1;
       }
-
-      if(n1.classA==n2.classA){
-        if(n1.pointsA>n2.pointsA){
+      if (n1.classA==n2.classA) {
+        if (n1.pointsA>n2.pointsA) {
           return 1;
         }
-
-        if(n1.pointsA<n2.pointsA){
+        if (n1.pointsA<n2.pointsA) {
           return -1;
         }
       }
       return 0;
     });
   }
-  public sortByClassARev(){
-    this.switchSortA()
+  public sortByClassARev() {
+    this.switchSortA();
     this.punkteStand = this.punkteStand.sort((n1,n2)=>{
       if(n1.classA>n2.classA){
         return 1;
@@ -134,7 +130,6 @@ export class TeamComponent implements OnInit {
       return 0;
     });
   }
-  
   public sortByClassB(){
     this.switchSortB()
     this.punkteStand = this.punkteStand.sort((n1,n2)=>{
@@ -158,7 +153,7 @@ export class TeamComponent implements OnInit {
     });
   }
   public sortByClassBRev(){
-    this.switchSortB()
+    this.switchSortB();
     this.punkteStand = this.punkteStand.sort((n1,n2)=>{
       if(n1.classB>n2.classB){
         return 1;
