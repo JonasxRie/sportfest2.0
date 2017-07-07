03b79dd19f12d858f58683bdba56670bf0bddac0
David Wennemaring <david.wennemaring@s-atiw>
2017-07-07 08:08:21 +0200

    Redirect bei erfolgreichem Anlegen einer Disziplin

--- sportfest/src/app/admin/create-discipline/create-discipline.component.ts
+++ sportfest/src/app/admin/create-discipline/create-discipline.component.ts
@@ -1,4 +1,4 @@
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Variable, Regel, Disziplin } from '../../interfaces';
 import { SportfestService } from '../../sportfest.service';
 import { Component, OnInit } from '@angular/core';
@@ -8,29 +8,29 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './create-discipline.component.html',
   styleUrls: ['./create-discipline.component.css']
 })
-export class CreateDisciplineComponent implements OnInit {    
-    sportartID: number;
-    sportart: string;
-    beschreibung: string;
-    minTeilnehmeranzahl: number;
-    maxTeilnehmeranzahl: number;
-    teamleistung: boolean = false;
-    kontrahentenAnzahl: number;
-    
-    datentypen: any[];
-    
-    secondVisible = true;    
-    rulesVar: Array<Variable>;
-    rules: Array<Regel>;
-    dummynumber: number;
+export class CreateDisciplineComponent implements OnInit {
+  sportartID: number;
+  sportart: string;
+  beschreibung: string;
+  minTeilnehmeranzahl: number;
+  maxTeilnehmeranzahl: number;
+  teamleistung: boolean = false;
+  kontrahentenAnzahl: number;
 
-  constructor(private sfService: SportfestService, private route: ActivatedRoute) { }
+  datentypen: any[];
+
+  secondVisible = true;
+  rulesVar: Array<Variable>;
+  rules: Array<Regel>;
+  dummynumber: number;
+
+  constructor(private sfService: SportfestService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.route.params.forEach((params: Params) => {
       this.sportartID = +params['did'];
     });
-    if(this.sportartID){
+    if (this.sportartID) {
       // Disziplin holen (beim Ändern)
       this.sfService.disziplin(this.sportartID).subscribe((data: Disziplin) => {
         this.sportart = data.name;
@@ -40,8 +40,8 @@ export class CreateDisciplineComponent implements OnInit {
         this.teamleistung = data.teamleistung;
         this.kontrahentenAnzahl = data.kontrahentenAnzahl;
         this.rules = [];
-        for(let i = data.regeln.length - 1; i>=0;i--){
-          this.rules[(data.regeln.length - 1)-i] = data.regeln[i];
+        for (let i = data.regeln.length - 1; i >= 0; i--) {
+          this.rules[(data.regeln.length - 1) - i] = data.regeln[i];
         }
         this.rulesVar = data.variablen;
         if (!this.rules) {
@@ -51,10 +51,10 @@ export class CreateDisciplineComponent implements OnInit {
           this.rulesVar = []
         }
       },
-      (err) => {
-        console.error('GET-Service "disziplin(sportartID)" not reachable.');
-      });
-    }else{
+        (err) => {
+          console.error('GET-Service "disziplin(sportartID)" not reachable.');
+        });
+    } else {
       this.rules = [];
       this.rulesVar = []
       this.addNewRuleVarLine();
@@ -71,11 +71,11 @@ export class CreateDisciplineComponent implements OnInit {
       }
     )
   }
-  
+
   public sendToBackend() {
     // Idizees der Regeln setzen
     for (let i = 0; i < this.rules.length; i++) {
-        this.rules[i].index = (i + 1) + '';
+      this.rules[i].index = (i + 1) + '';
     }
     let disziplinDTO = {
       name: this.sportart,
@@ -86,10 +86,10 @@ export class CreateDisciplineComponent implements OnInit {
       teamleistung: this.teamleistung,
       kontrahentenAnzahl: this.kontrahentenAnzahl,
       variablen: this.rulesVar,
-      regeln: this.rules,  
+      regeln: this.rules,
     }
     console.log("disziplinDTO", disziplinDTO);
-    if(this.sportartID){
+    if (this.sportartID) {
       this.sfService.disziplinAendern(this.sportartID, disziplinDTO).subscribe(
         (data) => {
           console.log(data);
@@ -98,10 +98,15 @@ export class CreateDisciplineComponent implements OnInit {
           console.log(err);
         }
       );
-    }else{
+    } else {
       this.sfService.disziplinSchreiben(disziplinDTO).subscribe(
         (data) => {
           console.log(data);
+          if (data.teamleistung)
+            this.router.navigate(["team/"+data.did+"/"+data.name]);
+          else
+            this.router.navigate(["einzel/"+data.did+"/"+data.name]);
+
         },
         (err) => {
           console.log(err);
@@ -109,36 +114,37 @@ export class CreateDisciplineComponent implements OnInit {
       );
     }
   }
-  
+
   addNewRuleVarLine() {
-    let line: Variable = { 
-      var_id:0,
-      name:'',
-      expressionParameter:'',
-      desc:'',
-      typ: 
-        { 
-          tid: ''
-        }
+    let line: Variable = {
+      var_id: 0,
+      name: '',
+      expressionParameter: '',
+      desc: '',
+      typ:
+      {
+        tid: ''
+      }
     }
     this.rulesVar.push(line);
   }
-  
-  removeRuleVarLine(index: number){
-      this.rulesVar.splice(index, 1);
+
+  removeRuleVarLine(index: number) {
+    this.rulesVar.splice(index, 1);
   }
 
-  addNewRuleLine(){
-    let rule: Regel = { 
-      expression: '', 
-      points: this.dummynumber};
+  addNewRuleLine() {
+    let rule: Regel = {
+      expression: '',
+      points: this.dummynumber
+    };
     this.rules.push(rule);
   }
-  
-  removeRuleLine(index: number){
+
+  removeRuleLine(index: number) {
     this.rules.splice(index, 1);
   }
-  
+
   submit() {
     this.secondVisible = true;
   }
